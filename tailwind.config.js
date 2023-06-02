48b198e302d8c11f88dc5756bcdcd48c7a248e92
ashotbes <ashot.bes@gmail.com>
2023-06-02 16:21:51 +0400

    deploying

---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  mode: "jit",
+  theme: {
+    extend: {
+      animation: {
+        'bounce-slow': 'bounce 2s infinite ',
+        'bounce-fast': 'bounce 0.5s infinite',
+      },
+      keyframes: {
+        'bounce': {
+          '0%, 100%': { transform: 'translateY(-10%)', animationTimingFunction: 'cubic-bezier(0.6, 0, 1, 1)' },
+          '50%': { transform: 'none', animationTimingFunction: 'cubic-bezier(0, 0, 0.2, 1)' },
+        },
+      },
+      colors: {
+        primary: "#00040f",
+        secondary: "#00f6ff",
+        dimWhite: "rgba(255, 255, 255, 0.7)",
+        dimBlue: "rgba(9, 151, 124, 0.1)",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
+    screens: {
+      xs: "480px",
+      ss: "620px",
+      sm: "768px",
+      md: "1060px",
+      lg: "1200px",
+      xl: "1700px",
+    },
+  },
+  plugins: [require('tailwindcss-animated'),require('taos/plugin')],
+};
